28dd362aa9df9dc45e7d906845164b09cf121254
Lucas Azevedo <lucasazevedooliveira@outlook.com.br>
2024-01-09 06:56:22 -0300

    css improve

--- components/div.tsx
+++ components/div.tsx
@@ -1,11 +1,12 @@
 import styles from './div.module.css'
 
 interface IProps {
+    button: boolean;
     children: React.ReactNode;
 }
 
-export default function Div({children}: IProps) {
+export default function Div({children, button}: IProps) {
     return (
-        <div className={styles.default}>{children}</div>
+        <div className={button ? styles.button : styles.default}>{children}</div>
     )
 }
--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Data } from '../../types/data';
 import { Pokemon } from '../../types/pokemon';
 import Div from '../../components/div';
+import styles from './game.module.css'
 
 export default function Game() {
   const [pokemon, setPokemon] = useState<Pokemon>();
@@ -9,6 +10,8 @@ export default function Game() {
   const [current, setCurrent] = useState(0);
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [pokemonsList, setPokemonsList] = useState<Pokemon[]>([]);
+
+  const [loading, setLoading] = useState<boolean>(true);
 
 
   useEffect(() => {
@@ -23,6 +26,7 @@ export default function Game() {
   }, [])
 
   const newQuiz = (data: Pokemon[]) => {
+    setLoading(true);
     let pokemonsRandom: Pokemon[] = [];
 
     const numbers = Array(100).fill(0).map((_, index) => index + 1);
@@ -36,6 +40,8 @@ export default function Game() {
     let number = randomNumberInRange(0, 3);
     setPokemonNumber(data.indexOf(pokemonsRandom[number]));
     setPokemon(pokemonsRandom[number]);
+
+    setLoading(false);
   }
 
   const randomNumberInRange = (min: number, max: number) => {
@@ -53,20 +59,26 @@ export default function Game() {
   }
 
     return (
-      <div className="wrapper">
-        {pokemonNumber > 0 ? 
-          <div>
-            <Div ><h2>Number is: {current}</h2></Div>
+      <div className={styles.game}>
+        {!loading ? 
+          <div className={styles.quiz}>
+            <div className={styles.pokemonImage}>
+              <img className={styles.image} width={300} height={300} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonNumber + 1}.svg`} alt="" />
+            </div>
+            
+            {/* <Div ><h2>Number is: {current}</h2></Div>
             <Div ><h2>Number is: {pokemon?.name}</h2></Div>
             <Div >
               <button onClick={() => newQuiz(pokemonsList)}>
                 Click Me Generate
               </button>
-            </Div>
+            </Div> */}
+
+            <Div button={false} >Who is that Pokémon ?</Div>
             
             
-            <div>{pokemons.map((item) => <Div key={item.name}><button onClick={() => {chooseOption(item)}} key={item.name}>{item.name}</button></Div>)}</div>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonNumber + 1}.svg`} alt="" />
+            <div className={styles.answers}>{pokemons.map((item) => <Div button={true} key={item.name}><button onClick={() => {chooseOption(item)}} key={item.name}>{item.name}</button></Div>)}</div>
+            
           </div>
           : <p>loading...</p>
         }
